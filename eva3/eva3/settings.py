5c69be02c366867a9d18769bf7e6a233aa6c8eed
Jotaa <Juanagustindefreitas@gmail.com>
2025-10-31 19:20:23 -0300

    si

--- eva3/eva3/settings.py
+++ eva3/eva3/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gestor_artistico',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'eva3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Eva3',
+        'USER': 'django_user',
+        'PASSWORD': '2405',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
